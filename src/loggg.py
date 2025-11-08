1ca798a9d1b9f8e81381a587c0698c19df077aa8
polisha <polinazuravlik2@gmail.com>
2025-11-08 08:02:04 +0300

    apdate main

--- src/loggg.py
+++ src/loggg.py
@@ -3,12 +3,25 @@
 
 import logging
 import sys
+from const import LOG_FILE
+
+
 
 #настройка логгирования
 logging.basicConfig(
-    
+    filename=LOG_FILE,           # файл, где идет запись логгов
+    level=logging.INFO,          # уровень логгирования
+    format='[%(asctime)s] %(levelname)s: %(message)s',  # формат записи
+    datefmt='%Y-%m-%d %H:%M:%S',  # дата фремя
+    encoding='utf-8'              # поддержка алф
 )
 
+
+# вывод в консоль для ошибок
+console_handler = logging.StreamHandler(sys.stderr)
+console_handler.setLevel(logging.ERROR)
+console_handler.setFormatter(logging.Formatter('[%(asctime)s] ERROR: %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+logging.getLogger().addHandler(console_handler)
 
 
 #запись сообщения в лог-файл, логгирование успешных команд
